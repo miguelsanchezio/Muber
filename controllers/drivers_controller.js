8c2b61e67f40c99c0a37d32ebda611f410061280
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-05-16 12:37:12 -0700

    Created drivers create controller and wrote its respective test.

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 module.exports = {
   greeting(req, res) {
     res.send({ hi: 'there' });
-  }
+  },
 
   create(req, res) {
-    
+    console.log(req.body);
+    res.send({ hi: 'there' });
   }
 };
---
+++ test/controllers/driver_controllers.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Drivers controller', () => {
+  it('POST to /api/drivers creates a new driver', done => {
+    request(app)
+      .post('/api/drivers')
+      .send({ email: 'test@test.com' })
+      .end((err, res) => {
+        assert(res.body.hi === 'there');
+        done();
+      });
+  });
+});
